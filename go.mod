5c1edd2063c7b4ff6cb18eb30217dcde82d6b23d
Aditya Sirish A Yelgundhalli <ayelgundhall@bloomberg.net>
2024-06-07 10:08:33 -0400

    Add initial implementation of webhook
    This is a rather hacky implementation that handles two types of events: pull
    requests and pull request reviews. Eventually, we'll want to clean this up,
    possibly to use APIs we expose in gittuf.
    
    The rough structure is based off of octo-sts/app's webhook by
    https://github.com/wlynch.
    
    Signed-off-by: Aditya Sirish <aditya@saky.in>
    Co-authored-by: Billy Lynch <billy@chainguard.dev>

---
+++ go.mod
@@ -0,0 +1,49 @@
+module github.com/gittuf/github-app
+
+go 1.22.2
+
+require (
+	cloud.google.com/go/kms v1.15.8
+	cloud.google.com/go/secretmanager v1.11.5
+	github.com/bradleyfalzon/ghinstallation/v2 v2.10.1-0.20240507094914-84bb4cbb7874
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/google/go-github/v61 v61.0.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/octo-sts/app v0.2.1-0.20240522225530-8cfa727a2584
+	golang.org/x/crypto v0.21.0
+)
+
+require (
+	cloud.google.com/go/auth v0.2.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.0 // indirect
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	cloud.google.com/go/iam v1.1.7 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.3 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.50.0 // indirect
+	go.opentelemetry.io/otel v1.25.0 // indirect
+	go.opentelemetry.io/otel/metric v1.25.0 // indirect
+	go.opentelemetry.io/otel/trace v1.25.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/oauth2 v0.19.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/api v0.174.0 // indirect
+	google.golang.org/genproto v0.0.0-20240311173647-c811ad7063a7 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240314234333-6e1732d8331c // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240415180920-8c6c420018be // indirect
+	google.golang.org/grpc v1.63.2 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
---
+++ internal/webhook/webhook.go
@@ -0,0 +1,341 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package webhook
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"mime"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"reflect"
+	"strings"
+	"sync"
+
+	"github.com/bradleyfalzon/ghinstallation/v2"
+	"github.com/google/go-github/v61/github"
+)
+
+/*
+	This is heavily inspired by the webhook in
+	https://github.com/chainguard-dev/octo-sts written by @wlynch.
+*/
+
+const (
+	reviewStateApproved = "approved"
+	reviewTypeSubmitted = "submitted"
+	reviewTypeDismissed = "dismissed"
+	DefaultGitHubURL    = "https://api.github.com"
+)
+
+type EnvConfig struct {
+	Port               int    `envconfig:"PORT" required:"true" default:"8080"`
+	KMSKey             string `envconfig:"KMS_KEY" required:"true"`
+	UserSigns          bool   `envconfig:"GET_USER_SIGNATURE" required:"true"`
+	IDProviderEndpoint string `envconfig:"ID_PROVIDER_ENDPOINT" default:"https://github.com/login/oauth"`
+	AppID              int64  `envconfig:"GITHUB_APP_ID" required:"true"`
+	WebhookSecret      string `envconfig:"GITHUB_WEBHOOK_SECRET" required:"true"`
+	GitHubURL          string `envconfig:"GITHUB_URL" default:"https://api.github.com"`
+	AppEmailID         string `envconfig:"APP_EMAIL_ID" required:"true"`
+	DevMode            bool   `envconfig:"DEV_MODE" default:"false"` // in dev mode, we use a key stored on disk
+}
+
+type GittufApp struct {
+	Transport     *ghinstallation.AppsTransport
+	WebhookSecret [][]byte
+	Params        *EnvConfig
+
+	init sync.Once
+}
+
+func (g *GittufApp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	g.init.Do(func() {
+		// maybe do this in docker file?
+		cmd := exec.Command("git", "config", "--global", "user.name", "gittuf-github-app")
+		if err := cmd.Run(); err != nil {
+			panic(err)
+		}
+
+		cmd = exec.Command("git", "config", "--global", "user.email", g.Params.AppEmailID)
+		if err := cmd.Run(); err != nil {
+			panic(err)
+		}
+
+		if g.Params.DevMode {
+			cmd = exec.Command("git", "config", "--global", "user.signingkey", g.Params.KMSKey)
+			if err := cmd.Run(); err != nil {
+				panic(err)
+			}
+
+			cmd = exec.Command("git", "config", "--global", "gpg.format", "ssh")
+			if err := cmd.Run(); err != nil {
+				panic(err)
+			}
+		}
+	})
+
+	log.Default().Printf("Serving app...")
+
+	payload, err := g.validatePayload(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Default().Printf("Validated payload, received: %s", string(payload))
+
+	eventType := github.WebHookType(r)
+	event, err := github.ParseWebHook(eventType, payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Default().Printf("Event type: %s", reflect.TypeOf(event).String())
+
+	switch event := event.(type) {
+	case *github.PullRequestEvent:
+		if err := g.handlePullRequest(r.Context(), event); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+	case *github.PullRequestReviewEvent:
+		if err := g.handlePullRequestReview(r.Context(), event); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (g *GittufApp) validatePayload(r *http.Request) ([]byte, error) {
+	// Taken from github.ValidatePayload - we can't use this directly since the body is consumed.
+	signature := r.Header.Get(github.SHA256SignatureHeader)
+	if signature == "" {
+		signature = r.Header.Get(github.SHA1SignatureHeader)
+	}
+	contentType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, s := range g.WebhookSecret {
+		payload, err := github.ValidatePayloadFromBody(contentType, bytes.NewBuffer(body), signature, s)
+		if err == nil {
+			return payload, nil
+		}
+	}
+	return nil, errors.New("no matching secrets")
+}
+
+// handlePullRequest creates an RSL entry when a pull request is merged for the
+// base branch.
+func (g *GittufApp) handlePullRequest(ctx context.Context, event *github.PullRequestEvent) error {
+	owner := event.GetRepo().GetOwner().GetLogin()
+	repository := event.GetRepo().GetName()
+	installationID := event.GetInstallation().GetID()
+
+	log.Default().Printf("Action on %s/%s#%d, installation of app %d", owner, repository, *event.PullRequest.Number, installationID)
+
+	if !*event.PullRequest.Merged {
+		// Nothing to do
+		return nil
+	}
+
+	transport := ghinstallation.NewFromAppsTransport(g.Transport, installationID)
+	token, err := transport.Token(ctx)
+	if err != nil {
+		return err
+	}
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	cloneURL := *event.PullRequest.Base.Repo.CloneURL
+
+	parsedURL, err := url.Parse(cloneURL)
+	if err != nil {
+		return err
+	}
+	parsedURL.User = url.UserPassword("x-access-token", token)
+	cloneURL = parsedURL.String()
+
+	localDirectory, err := os.MkdirTemp("", "gittuf")
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("git", "clone", "--branch", *event.PullRequest.Base.Ref, cloneURL, localDirectory)
+	if err := cmd.Run(); err != nil {
+		log.Default().Print("clone: " + err.Error())
+		return err
+	}
+	// this can be removed once full git binary support lands, control via GIT_DIR env
+	if err := os.Chdir(localDirectory); err != nil {
+		return err
+	}
+	defer os.Chdir(pwd) //nolint:errcheck
+
+	cmd = exec.Command("git", "fetch", "origin", "refs/gittuf/*:refs/gittuf/*")
+	if err := cmd.Run(); err != nil {
+		log.Default().Print("fetch gittuf and base: " + err.Error())
+		return err
+	}
+
+	cmd = exec.Command("/app/gittuf", "rsl", "record", *event.PullRequest.Base.Ref)
+	if err := cmd.Run(); err != nil {
+		log.Default().Print("rsl entry creation: " + err.Error())
+		return err
+	}
+
+	cmd = exec.Command("git", "push", "origin", "refs/gittuf/*")
+	if err := cmd.Run(); err != nil {
+		log.Default().Print("push gittuf: " + err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// handlePullRequestReview observes pull request review events. If a pull
+// request is approved, it records an attestation with the details of the
+// approver as well as the nature of the change (updated base branch, base
+// commit, target tree ID). If an approval is dismissed, it updates the
+// corresponding attestation to indicate the approver has changed their mind.
+func (g *GittufApp) handlePullRequestReview(ctx context.Context, event *github.PullRequestReviewEvent) error {
+	owner := event.GetRepo().GetOwner().GetLogin()
+	repository := event.GetRepo().GetName()
+	installationID := event.GetInstallation().GetID()
+
+	log.Default().Printf("Review on %s/%s#%d, installation of app %d", owner, repository, *event.PullRequest.Number, installationID)
+
+	transport := ghinstallation.NewFromAppsTransport(g.Transport, installationID)
+	token, err := transport.Token(ctx)
+	if err != nil {
+		return err
+	}
+
+	client := github.NewClient(&http.Client{
+		Transport: transport,
+	})
+
+	if g.Params.GitHubURL != DefaultGitHubURL {
+		log.Default().Print("Enterprise instance found, creating enterprise client")
+		log.Default().Printf("Instance url: %s", g.Params.GitHubURL)
+		base := fmt.Sprintf("%s/%s/%s/", g.Params.GitHubURL, "api", "v3")
+		upload := fmt.Sprintf("%s/%s/%s", g.Params.GitHubURL, "api", "uploads")
+		client, err = client.WithEnterpriseURLs(base, upload)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Who was the approver?
+	reviewer := event.Review.GetUser()
+	// TODO: don't hardcode this as a sigstore approver right now
+	approver := fmt.Sprintf("fulcio:%s::%s", *reviewer.Login, g.Params.IDProviderEndpoint)
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	cloneURL := *event.PullRequest.Base.Repo.CloneURL
+
+	parsedURL, err := url.Parse(cloneURL)
+	if err != nil {
+		return err
+	}
+	parsedURL.User = url.UserPassword("x-access-token", token)
+	cloneURL = parsedURL.String()
+
+	localDirectory, err := os.MkdirTemp("", "gittuf")
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("git", "clone", "-b", *event.PullRequest.Base.Ref, cloneURL, localDirectory)
+	if err := cmd.Run(); err != nil {
+		log.Default().Print("clone: " + err.Error())
+		return err
+	}
+	// this can be removed once full git binary support lands, control via GIT_DIR env
+	if err := os.Chdir(localDirectory); err != nil {
+		return err
+	}
+	defer os.Chdir(pwd) //nolint:errcheck
+
+	// Fetch feature ref
+	cmd = exec.Command("git", "fetch", "origin", fmt.Sprintf("%s:%s", *event.PullRequest.Head.Ref, *event.PullRequest.Head.Ref))
+	if err := cmd.Run(); err != nil {
+		log.Default().Print("fetch feature branch: " + err.Error())
+		return err
+	}
+
+	var message string
+	// Fetch gittuf refs
+	cmd = exec.Command("git", "fetch", "origin", "refs/gittuf/*:refs/gittuf/*")
+	if err := cmd.Run(); err != nil {
+		log.Default().Print("fetch gittuf: " + err.Error())
+		return err
+	}
+
+	if *event.Action == reviewTypeSubmitted {
+		args := []string{"dev", "add-github-approval", "--repository", owner + "/" + repository, "--pull-request-number", fmt.Sprintf("%d", *event.PullRequest.Number), "--review-ID", fmt.Sprintf("%d", *event.Review.ID), "--approver", approver, "-k", g.Params.KMSKey}
+		log.Default().Print("cmd: ", strings.Join(args, " "))
+		cmd = exec.Command("/app/gittuf", args...)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("GITHUB_BASE_URL=%s", g.Params.GitHubURL), fmt.Sprintf("GITHUB_TOKEN=%s", token))
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Default().Print("gittuf attest: " + err.Error() + " " + string(output))
+			return err
+		}
+
+		message = fmt.Sprintf("Observed review from %s, @%s", approver, *reviewer.Login)
+	} else if *event.Action == reviewTypeDismissed {
+		args := []string{"dev", "dismiss-github-approval", "--review-ID", fmt.Sprintf("%d", *event.Review.ID), "--dismiss-approver", approver, "-k", g.Params.KMSKey}
+		log.Default().Print("cmd: ", strings.Join(args, " "))
+		cmd = exec.Command("/app/gittuf", args...)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("GITHUB_BASE_URL=%s", g.Params.GitHubURL), fmt.Sprintf("GITHUB_TOKEN=%s", token))
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Default().Print("gittuf attest: " + err.Error() + " " + string(output))
+			return err
+		}
+
+		message = fmt.Sprintf("Observed dismissal of review by %s", approver)
+	}
+
+	cmd = exec.Command("git", "push", "origin", "refs/gittuf/*")
+	if err := cmd.Run(); err != nil {
+		log.Default().Print("push gittuf: " + err.Error())
+		return err
+	}
+
+	log.Default().Printf("Created message: %s", message)
+
+	commentCreated, response, err := client.Issues.CreateComment(ctx, owner, repository, *event.PullRequest.Number, &github.IssueComment{
+		Body: &message,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to create GitHub comment: %w", err)
+	} else {
+		log.Printf("Comment created: %s", *commentCreated.Body)
+		log.Printf("Response: %s", response.Status)
+	}
+
+	log.Default().Println("Commented!")
+
+	return nil
+}
